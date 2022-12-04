5ceb128d0ca6fb14ac6245c572c4da6732b6cdd6
Hara <harachung90@gmail.com>
2022-12-04 15:15:54 +1100

    Third commit. Added a property for isPun (boolean value)

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ function App() {
     <div className="App">
         <Jokes
             punchline="It's hard to explain puns to kleptomaniacs because they always take things literally."
+            isPun={true}
+            upvotes={10}
+            downvotes={2}
+            comments={[{author: "", body: "", title: ""}]}
         />
         <Jokes
             setup="I got my daughter a fridge for her birthday."
             punchline="I can't wait to see her face light up when she opens it."
+            isPun={false}
         />
         <Jokes
             setup="How did the hacker escape the police?"
             punchline="He just ransomware!"
+            isPun={true}
         />
         <Jokes
             setup="Why don't pirates travel on mountain roads?"
             punchline="Scurvy."
+            isPun={true}
         />
     </div>
   );
